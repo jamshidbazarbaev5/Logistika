3167a9ad89e2b8ee45f7aa215b89cd7b6197e7f2
jamshidbazarbaev5 <your-github-email@example.com>
2025-02-04 00:42:29 +0500

    fixed 404 error

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true
+  }
 
 })
