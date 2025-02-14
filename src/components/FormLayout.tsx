dccf415b1006c965aec2e58e37d328ba5ec80f46
jamshidbazarbaev5 <your-github-email@example.com>
2025-02-14 19:27:48 +0500

    added VIP feature

--- src/components/FormLayout.tsx
+++ src/components/FormLayout.tsx
@@ -8,7 +8,7 @@ export default function FormLayout({
     children: React.ReactNode;
   }) {
     return (
-      <div className="min-h-screen p-4 sm:p-6 lg:p-8 bg-gray-50 dark:bg-gray-900">
+      <div className="min-h-screen p-4 sm:p-6 lg:p-8     dark:bg-gray-900">
         <div className="max-w-7xl mx-auto">
           <div className="mb-6 sm:mb-8">
             <h1 className="text-xl sm:text-2xl lg:text-3xl font-semibold text-gray-900 dark:text-white">
